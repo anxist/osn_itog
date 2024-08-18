9a1fa75b3550ae2ebec7cd1ba8587ff543b4b901
Diana Narizhnyaya <82086491+anxist@users.noreply.github.com>
2024-08-19 00:00:32 +0300

    Перечитала задание, измениал код и описание

--- index.py
+++ index.py
@@ -1,14 +1,26 @@
 def filter_short_strings(input_array):
     result = []
+    
     for item in input_array:
         if len(item) <= 3:
             result.append(item)
+    
     return result
 
-input_array1 = ["Hello", "2", "world", ":-)"]
-input_array2 = ["1234", "1567", "-2", "computer science"]
-input_array3 = ["Russia", "Denmark", "Kazan"]
+def main():
+    print("Введите строки, разделенные пробелами. Введите 'стоп' для завершения ввода.")
+    input_array = []
+    
+    while True:
+        line = input()
+        if line.lower() == 'стоп':
+            break
+        input_array.append(line)
+    
+    filtered_array = filter_short_strings(input_array)
+    
+    print("Отфильтрованный массив:")
+    print(filtered_array)
 
-print(filter_short_strings(input_array1))  #  ['2', ':-)']
-print(filter_short_strings(input_array2))  #  ['-2']
-print(filter_short_strings(input_array3))  #  []
+if __name__ == "__main__":
+    main()
